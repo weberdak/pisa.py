e2b9052af24303b2b8db0203534fe543f8a5d847
Daniel Weber <dweber@umn.edu>
2019-11-09 13:16:09 -0600

    Multiprocessing now supported.

---
+++ examples/sarcolipin_multiproc/sln_fit.sh
@@ -0,0 +1,10 @@
+python ../../pisa.py \
+       --peaks assignments.dat \
+       --flip 90.0 \
+       --rho_start 6 \
+       --fit_exclude 1 3 6 7 8 9 10 11 12 13 28 29 31 \
+       --fit_tau 0.0 90.0 1.0 \
+       --fit_rho0 0.0 360.0 4.0 \
+       --fit_order 0.80 0.90 0.05 \
+       --aCaNH 116.0 \
+       --explore
